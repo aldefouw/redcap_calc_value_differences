bdf461c0b27650bf452e78194b3b405dd8063c2e
Adam De Fouw <aldefouw@medicine.wisc.edu>
2017-09-14 10:21:15 -0500

    Sort the columns properly for an arm

--- library/redcap_versions/redcap_712.rb
+++ library/redcap_versions/redcap_712.rb
@@ -74,14 +74,16 @@ module Redcap712
     event_row.search('th').each_with_index do |th, i|
       if th.attributes.key?("colspan")
 
-        current_text = th.text.split("(Arm 1")[0].strip
-        num_of_cols = th.attributes["colspan"].value
+        current_text = th.text.split("(Arm ")[0].strip
+        current_arm = th.text.split("(Arm ")[1].split(":")[0].to_i
+        num_of_cols = th.attributes["colspan"].value.to_i
 
         @event_forms.each_with_index do |form, index|
           current_event = @define_my_events[form["unique_event_name"]]
+          arm = form["unique_event_name"].split("_arm_")[1].to_i
 
-          if current_text == current_event[:name]
-            @event_cols[form["unique_event_name"]] = { cols: num_of_cols, name: current_event[:name] }
+          if current_text == current_event[:name] && current_arm == arm
+            @event_cols[form["unique_event_name"]] = { arm: arm, cols: num_of_cols, name: current_event[:name] }
           end
         end
       end
@@ -89,20 +91,22 @@ module Redcap712
 
     header.search('th').each { |th| @instrument_mappings << th.text.strip.downcase.parameterize.underscore }
 
-    count = 0
-
     @event_cols.each_with_index do |e, index|
-      e[1][:arm] = e[0].split("_arm_")[1].to_i
       e[1][:index] = index
     end
 
     #Sort by the arm first and then the index (just like the table headers)
     @event_cols = @event_cols.sort_by { |k|  [ k[1][:arm], k[1][:index] ] }.to_h
 
-    @event_cols.each_with_index do |e, i|
+    count = 0
+
+    @event_cols.each_with_index do |e|
       e[1][:forms] = []
-      (0..number_of_cols(e)).each { |i| e[1][:forms] << { count = count + 1 => @instrument_mappings[count - 1] }  }
+      (0..number_of_cols(e)).each { e[1][:forms] << { count = count + 1 => @instrument_mappings[count - 1] } }
     end
+
+    binding.pry
+
   end
 
   def find_active_records(id:, instrument:)
